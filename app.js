b9898035bc577aedbc11cc5a3c36f932215b425d
Sandesh001211 <dongaonkarsandesh001@gmail.com>
2026-01-09 14:12:02 +0530

    log request

--- app.js
+++ app.js
@@ -14,9 +14,14 @@ app.get("/", (req, res) => {
   res.send("Phoebee Backend is running 🚀");
 });
 
+const log_request = async (req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next()
+};
+
 // 🔗 API ROUTES
 import resolutionRoutes from "./routes/resolutionRoutes.js";
-app.use("/api/resolutions", resolutionRoutes);
+app.use("/api/resolutions", log_request,resolutionRoutes);
 
 // 🔗 DB CONNECT
 mongoose
@@ -25,6 +30,4 @@ mongoose
   .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
